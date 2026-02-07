95d44b3e188ecc59d15f4018f7cd9692c8f97740
Kayaba0 <marcomartinelli40@yahoo.it>
2026-02-07 02:20:53 +0100

    Vercel Fix

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,9 +1,7 @@
 import express from "express";
-import "dotenv/config";
-import { registerRoutes } from "../server/routes";
+import { registerRoutes } from "./_routes";
 
 const app = express();
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
